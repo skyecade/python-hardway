02cad558cf419527c447635b66b80dea6d46aea1
Robert Sinclaire <robert@robertsinclaire.com>
2021-12-13 21:03:41 -0500

    test

--- ex5.py
+++ ex5.py
@@ -15,5 +15,4 @@ print(f"His teeth are usually {my_teeth} depending on the coffee.")
 
 # this line is tricky, try to get it exactly right
 total = my_age + my_height + my_weight
-
 print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
