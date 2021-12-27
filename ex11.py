5372fbbb87df7fee2f089a8bacbc429ae8609825
Robert Sinclaire <robert@robertsinclaire.com>
2021-12-27 06:42:39 -0500

    finished lessons 11 and 12

---
+++ ex11.py
@@ -0,0 +1,9 @@
+print("How old are you?", end=' ')
+age = input()
+print("How tall are you?", end=' ')
+height = input()
+print("How much do you weigh?", end=' ')
+weight = input()
+
+print(
+    f"So, you're {age} years old, {height} feet tall, and weigh {weight} pounds.")
---
+++ ex12.py
@@ -0,0 +1,6 @@
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you way? ")
+
+print(
+    f"So, you are {age} years old, {height} feet tall, and weigh {weight} pounds.")
